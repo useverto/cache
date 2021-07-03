a41bd6c3c0de881549d16cb4eb157abfda539ab6
Marton Lederer <marton@lederer.hu>
2021-07-03 16:07:22 +0200

    chore: update community contract

--- src/utils/verto.ts
+++ src/utils/verto.ts
@@ -11,7 +11,7 @@ const client = new Arweave({
 const gql = new ArDB(client);
 
 // community contract ID
-export const COMMUNITY_CONTRACT = "W_84yhLuUptuBi4qTUChiAE2wtHOOvTdqvj3IYfU6gg";
+export const COMMUNITY_CONTRACT = "yxdwrWJiaDDwKHIridjuBhtSR_IF7K8B8euJRLqaTrw";
 
 export const getTradingPosts = async (): Promise<string[]> => {
   const { state } = await Contract.findById(

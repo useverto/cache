5ccc2c22285eb087d98af94576de57197a5de89d
andreespirela <andreespirela@outlook.com>
2021-12-27 23:19:36 -0500

    Adding communities random

--- src/modules/sites/site.controller.ts
+++ src/modules/sites/site.controller.ts
@@ -1,6 +1,10 @@
 import {Controller, Get, Param, Query} from "@nestjs/common";
 import {TokensDatastoreService} from "../../inc/services/contracts-datastore/tokens-datastore.service";
-import {ArtFilter, CollectionsFilter} from "../../inc/services/contracts-datastore/common-filters/token-filters";
+import {
+    ArtFilter,
+    CollectionsFilter,
+    CommunitiesFilter
+} from "../../inc/services/contracts-datastore/common-filters/token-filters";
 import {QueryResult, QueryResultBase} from "../../inc/services/core/gcp-datastore/model";
 import {CommunityTokensDatastore} from "../../inc/services/core/gcp-datastore/kind-interfaces/ds-community-tokens";
 
@@ -37,6 +41,28 @@ export class SiteController {
         const finalEntities = [
             ...art.entities,
             ...collection.entities
+        ].sort(() => Math.random() - Math.random()).slice(0, intLimit);
+
+        return {
+            entities: finalEntities,
+            resultsStatus: finalEntities.length > 0 ? 'FOUND' : 'EMPTY'
+        }
+    }
+
+    @Get('communities/random')
+    public async getRandomCommunities(@Query('limit') limit: string): Promise<QueryResultBase<CommunityTokensDatastore>> {
+        let intLimit = parseInt(limit || '4');
+        intLimit = intLimit > 10 ? 10 : intLimit;
+
+        const communities = await this.tokensDatastoreService.queryTokens({
+            limit: intLimit,
+            filters: [
+                CommunitiesFilter
+            ]
+        });
+
+        const finalEntities = [
+            ...communities.entities
         ].sort(() => Math.random() - Math.random()).slice(0, intLimit);
 
         return {

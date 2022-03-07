6d5c7414caab11dad41bad3a3df216f3a28e75d9
andreespirela <andreespirela@outlook.com>
2022-03-07 10:13:05 -0500

    Refacotring skeleton saving process

--- src/modules/commons/common.module.ts
+++ src/modules/commons/common.module.ts
@@ -6,8 +6,9 @@ import {ContractWorkerService} from "../../inc/services/core/contract-worker/con
 import {GcpDatastoreService} from "verto-internals/services/gcp";
 import {ExceptionHandlerService} from "../../inc/services/core/handlers/exception-handler";
 import {RecoverableContractsDatastoreService} from "../../inc/services/contracts-datastore/recoverable-contracts-datastore.service";
+import {TokensDatastoreService} from "../../inc/services/contracts-datastore/tokens-datastore.service";
 
-const members = [ArweaveClientService, GcpStorageService, GcpContractStorageService, ContractWorkerService, GcpDatastoreService, ExceptionHandlerService, RecoverableContractsDatastoreService]
+const members = [ArweaveClientService, GcpStorageService, GcpContractStorageService, ContractWorkerService, GcpDatastoreService, ExceptionHandlerService, RecoverableContractsDatastoreService, TokensDatastoreService]
 
 @Module({
     providers: [...members],
